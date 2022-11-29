e9bce862d1c6840a910d63ef4f043e14a0674931
leolalopeslobo <leolalopeslobo@gmail.com>
2022-11-29 13:51:55 +0530

    taking input

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //taking input
+        System.out.println(in.nextInt());
+        System.out.println(in.next());
+        System.out.println(in.nextLine());
+    }
+}
