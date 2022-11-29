5e96271d88f2792e09e6e8e419bd9ac458b23d02
leolalopeslobo <leolalopeslobo@gmail.com>
2022-11-29 14:29:03 +0530

    primitives

---
+++ src/Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int numInt = 63;
+        char letter = 'l';
+        float numFloat = 92.63f;
+        double numDouble = 54673892.74536; //largeDecimalNumber
+        long numLong = 98463428759735L; //largeInteger
+        boolean checkBool = true;
+
+        //String in not Primitive
+        //Wrapper classes adds more functionality to the primitives
+    }
+}
